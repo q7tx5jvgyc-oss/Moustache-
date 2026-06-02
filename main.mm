dd4c9942bf661af9c0f19b38e68ed5c92508c2d0
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-02 06:05:23 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,85 +1,38 @@
 #import <UIKit/UIKit.h>
 
-static BOOL isEnabled = NO;
-static float speedValue = 1.0;
-
-@interface FloatingMenu : UIView
+@interface MoustacheView : UIView
 @end
 
-@implementation FloatingMenu
+@implementation MoustacheView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
 
     if (self) {
-        self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
-        self.layer.cornerRadius = 14;
+        self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8];
+        self.layer.cornerRadius = 12;
 
-        UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, 180, 20)];
-        title.text = @"Moustache Menu";
-        title.textColor = UIColor.whiteColor;
-        title.font = [UIFont boldSystemFontOfSize:14];
-        [self addSubview:title];
-
-        UIButton *start = [UIButton buttonWithType:UIButtonTypeSystem];
-        start.frame = CGRectMake(10, 35, 80, 30);
-        [start setTitle:@"Start" forState:UIControlStateNormal];
-        [start setTitleColor:UIColor.greenColor forState:UIControlStateNormal];
-        [start addTarget:self action:@selector(startAction) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:start];
-
-        UIButton *stop = [UIButton buttonWithType:UIButtonTypeSystem];
-        stop.frame = CGRectMake(100, 35, 80, 30);
-        [stop setTitle:@"Stop" forState:UIControlStateNormal];
-        [stop setTitleColor:UIColor.redColor forState:UIControlStateNormal];
-        [stop addTarget:self action:@selector(stopAction) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:stop];
-
-        UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(10, 75, 170, 20)];
-        slider.minimumValue = 0.5;
-        slider.maximumValue = 3.0;
-        slider.value = 1.0;
-        [slider addTarget:self action:@selector(sliderChanged:) forControlEvents:UIControlEventValueChanged];
-        [self addSubview:slider];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 200, 30)];
+        label.text = @"Moustache Loaded 🔥";
+        label.textColor = UIColor.whiteColor;
+        [self addSubview:label];
     }
 
     return self;
 }
 
-- (void)startAction {
-    isEnabled = YES;
-    NSLog(@"[Moustache] Started");
-}
-
-- (void)stopAction {
-    isEnabled = NO;
-    NSLog(@"[Moustache] Stopped");
-}
-
-- (void)sliderChanged:(UISlider *)slider {
-    speedValue = slider.value;
-    NSLog(@"Speed: %f", speedValue);
-}
-
-// سحب القائمة
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
-    CGPoint pos = [touch locationInView:self.superview];
-    self.center = pos;
-}
-
 @end
 
 __attribute__((constructor))
-static void init_tweak() {
+static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
 
         UIWindow *window = UIApplication.sharedApplication.keyWindow;
         if (!window) return;
 
-        FloatingMenu *menu = [[FloatingMenu alloc] initWithFrame:CGRectMake(60, 150, 200, 110)];
-        [window addSubview:menu];
+        MoustacheView *view = [[MoustacheView alloc] initWithFrame:CGRectMake(50, 150, 220, 80)];
+        [window addSubview:view];
 
-        NSLog(@"[Moustache] Loaded Successfully 🔥");
+        NSLog(@"Moustache injected");
     });
 }
